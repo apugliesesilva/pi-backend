bacfea2b4a41e00b4d3ea18920b3c4c2bee8f6d3
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-17 17:50:48 -0300

    feat: listen to a env variable port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvService } from './infra/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = app.get(EnvService).get('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
